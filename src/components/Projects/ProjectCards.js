3619eaee0cedd27a567728e05c2694cec21ca38b
Lucif3r-in <ashutosh123rath@gmail.com>
2023-06-04 17:07:44 +0530

    fix: project card

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -6,12 +6,22 @@ import { BsLink45Deg } from "react-icons/bs";
 
 function ProjectCards(props) {
   return (
-    <Card className="project-card-view">
+    <Card
+      className="project-card-view"
+      style={{ borderRadius: "20px", padding: "0px" }}
+    >
       <Card.Title
         style={{
           margin: "auto",
-          paddingBottom: "10px",
-          backgroundColor: "var(--text-primary)",
+          padding: "10px 0",
+          backgroundColor: "var(--secondary)",
+          width: "100%",
+          borderRadius: "20px 20px 0 0",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          fontSize: "25px",
+          fontStretch: "4px",
         }}
       >
         {props.title}
@@ -21,10 +31,15 @@ function ProjectCards(props) {
         src={props.imgPath}
         alt="card-img"
         className={props.isPhone}
-        style={{ border: "2px solid #fff" }}
+        style={{
+          border: "2px solid #fff",
+          width: "90%",
+          margin: "auto",
+          marginTop: "4%",
+        }}
       />
       <Card.Body>
-        <Card.Text style={{ textAlign: "justify" }}>
+        <Card.Text style={{ textAlign: "justify" }} className="">
           {props.description}
         </Card.Text>
         {props.isGh && (
